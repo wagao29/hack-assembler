aed873304752d7d525e6f1b9d3024c97d8ea0b05
wagao <wagao1029@gmail.com>
2020-09-22 12:32:19 +0900

    Fix advance

--- src/parser.rs
+++ src/parser.rs
@@ -5,6 +5,7 @@ use std::io::BufReader;
 pub struct Parser {
     pub stream: BufReader<std::fs::File>,
     pub current_command: String,
+    pub has_more_commands: bool,
 }
 
 impl Parser {
@@ -12,24 +13,28 @@ impl Parser {
         Parser {
             stream: f,
             current_command: String::new(),
+            has_more_commands: true,
         }
-    }
-
-    pub fn has_more_commands(&self) -> bool {
-        //後で実装
-        true
     }
 
     pub fn advance(&mut self) {
         loop {
             let mut buf = String::new();
             self.stream.read_line(&mut buf).unwrap();
+            //改行文字が含まれない、つまり入力終了
+            if buf.is_empty() {
+                self.has_more_commands = false;
+                break;
+            }
+            //改行文字、空白の削除
             buf = buf.trim().replace(" ", "").to_string();
+            //空白の行はスキップ
             if buf.is_empty() {
                 continue;
             }
+            //コメント行はスキップ
             match buf.find("//") {
-                Some(_) => (),
+                Some(_) => continue,
                 None => {
                     self.current_command = buf;
                     break;
@@ -53,5 +58,15 @@ mod tests {
         assert_eq!("@2", p.current_command);
         p.advance();
         assert_eq!("D=A", p.current_command);
+        p.advance();
+        assert_eq!("@3", p.current_command);
+        p.advance();
+        assert_eq!("D=D+A", p.current_command);
+        p.advance();
+        assert_eq!("@0", p.current_command);
+        p.advance();
+        assert_eq!("M=D", p.current_command);
+        p.advance();
+        assert_eq!(false, p.has_more_commands);
     }
 }
