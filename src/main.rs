a0c1836c95814366f7cb7eb5f449b636c8b48709
wagao <wagao1029@gmail.com>
2020-09-26 18:38:18 +0900

    Add file processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use hack_assembler::{code, parser};
+use std::io::prelude::*;
+use std::io::{BufReader, BufWriter};
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let input_file_name = &args[1];
+    let input_file = BufReader::new(fs::File::open(input_file_name).unwrap());
+
+    let output_file_name = input_file_name.replace("asm", "hack");
+    let mut output_file = BufWriter::new(fs::File::create(output_file_name).unwrap());
+
+    let b = "test".as_bytes();
+    output_file.write(b).unwrap();
+
+    let parser = parser::Parser::new(input_file);
 }
